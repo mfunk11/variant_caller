8549e980b221fd1fa09c3b1f1180adc97bc08e00
mfunk11 <100898964+mfunk11@users.noreply.github.com>
2023-02-20 12:25:08 -0500

    Added files because I could not push

--- mf_pkg/gatk.py
+++ mf_pkg/gatk.py
@@ -1,1 +1,30 @@
-:
+import os
+
+def run_mark_duplicates_spark(name, bam):
+    cmd = f"gatk MarkDuplicatesSpark -I {bam} -O {bam}.marked_duplicates -M {name}.sort.md.metricts.txt"
+    os.system(cmd)
+    return cmd
+
+def run_bqsr(dup_bam, ref, bed):
+    cmd = f"gatk BaseRecalibrator -I {dup_bam} -R{ref}, --known-sites {bed}, -O recal-table.table"
+    os.system(cmd)
+
+
+def run_qual_cab(dup_bam, ref, bed):
+    run_bqsr(dup_bam, ref, bed)
+    cmd = f"gatk ApplyBQSR -R {ref} -I {dup_bam} --bqsr-recal-file recal-table.table -O output.bam"
+    os.system(cmd)
+    return(cmd)
+
+def run_haplotype_caller(name, dup_bam, ref, bed):
+    cmd = f"gatk HaplotypeCaller -R {ref} -I {dup_bam} -O {name}.g.vcf.gz"
+    os.system(cmd)
+    return cmd
+
+def run_gatk(name, bam, ref, bed):
+    cmd1 = run_mark_duplicates_spark(name, bam)
+    print("Ran Mark Duplicates.")
+    cmd2 = run_qual_cab(bam, ref, bed)
+    print("Ran Base Recalibration")
+    cmd3 = run_haplotype_caller(name, bam, ref, bed)
+    print("GATK run successfully.")
--- mf_pkg/star.py
+++ mf_pkg/star.py
@@ -1,30 +1,32 @@
 import sys
 import os
 
+def run_star_no_gtf(ref, fastq_1, fastq_2):
+	if fastq_2 is None:
+		cmd = "star --runThreadN 4 --runMode alignReads --genomeDir . --readFilesIn {fastq}"
+	else:
+		cmd = f"star --runThreadN 4 --runMode alignReads --genomeDir . --readFilesIn {fastq_1} {fastq_2}"
 
-def run_star(gtf, ref):
-    if (len(sys.argv) != 4) and (len(sys.argv) != 5):
-        print("usage: [gtf][reference genome][fastq_1](fastq_2)")
-    else:
-        if len(sys.argv) == 4:
-            fastq_file = sys.argv[3]
-            cmd = f"star --runThreadN 6 --runMode genomeGenerate --genomeDir . --genomeFastaFiles {ref} --sjdbGTFfile {gtf} --sjdbOverhang 100"
-            print(cmd)
-            os.system(cmd)
+def run_star1(gtf, ref, fastq_file):
+            if gtf is None:
+		return run_star_no_gtf(ref, fastq_file, None)
+	    else:
+            	cmd = f"star --runThreadN 6 --runMode genomeGenerate --genomeDir . --genomeFastaFiles {ref} --sjdbGTFfile {gtf} --sjdbOverhang 100"
+            	cmd2 = f"star --runThreadN 4 --runMode alignReads --genomeDir . --readFilesIn {fastq_file}"
+            	os.system(cmd, cmd2)
+	    return cmd, cmd2
 
-            cmd2 = f"star --runThreadN 4 --runMode alignReads --genomeDir . --readFilesIn {fastq_file}"
-            os.system(cmd2)
+def run_star2(gtf, ref, fastq_1, fastq_2):
+            if gtf is None:
+		return run_star_no_gtf(ref, fastq_1, fastq_2)
+	    else:
+            	cmd = f"star --runThreadN 6 --runMode genomeGenerate --genomeDir . --genomeFastaFiles {ref} --sjdbGTFfile {gtf} --sjdbOverhang 100"
+            	os.system(cmd)
+            	cmd2 = f"star --runThreadN 4 --runMode alignReads --genomeDir . --readFilesIn {fastq_1} {fastq_2}"
+            	print(cmd)
+            	os.system(cmd2)
+        	return cmd, cmd2
 
-        elif len(sys.argv) == 5:
-            fastq_1 = sys.argv[3]
-            fastq_2 = sys.argv[4]
-            cmd = f"star --runThreadN 6 --runMode genomeGenerate --genomeDir . --genomeFastaFiles {ref} --sjdbGTFfile {gtf} --sjdbOverhang 100"
-            print(cmd)
-            os.system(cmd)
-            cmd2 = f"star --runThreadN 4 --runMode alignReads --genomeDir . --readFilesIn {fastq_1} {fastq_2}"
-            print(cmd)
-            os.system(cm
-        return cmd, cmd2
 
 
 
