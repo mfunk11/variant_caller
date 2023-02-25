bc2161a70986510a526d947bef6c88dd94d4c262
mfunk11 <100898964+mfunk11@users.noreply.github.com>
2023-02-24 23:46:30 -0500

    Add files via upload

--- tests/test_star.py
+++ tests/test_star.py
@@ -1,4 +1,5 @@
-from mf_pkg import star
+import mf_pkg.star
+from datetime import datetime
 import urllib.request
 import gzip
 import shutil
@@ -7,17 +8,30 @@ class TestSTAR(unittest.TestCase):
     
     def test_run_star2(self):
         fastq1_url = urllib.request.urlopen("ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR181/008/SRR18117708/SRR18117708_1.fastq.gz")
-        fastq2_url = urllib.request.urlopen("ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR181/008/SRR18117708/SRR18117708_2.fastq.gz") 
+	print(datetime.now() + ' First file read in.')
+        fastq2_url = urllib.request.urlopen("ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR181/008/SRR18117708/SRR18117708_2.fastq.gz")
+	print(datetime.now() + ' Second file read in.') 
         with gzip.open(fastq1_url, 'rb') as f_in:
             with open("SRR18117708_1.fastq", 'wb') as f_out:
                 shutil.copyfileobj(f_in, f_out)
-            with open("SRR19117708_2.fastq", 'wb') as f_out:
-                shutil.copyfileobj(f_in, f_out) 
+	print(datetime.now() + ' First fastq file copied.')
+	with gzip.open(fastq2_url, 'rb') as f_in2:
+            with open("SRR19117708_2.fastq", 'wb') as f_out2:
+                shutil.copyfileobj(f_in2, f_out2)
+	print(datetime.now() + ' Second fastq file copied.') 
         gtf = urllib.request.urlopen('https://www.arabidopsis.org/download_files/Genes/TAIR10_genome_release/TAIR10_chromosome_files/TAIR10_chr_all.fas.gz')
         ref_url = urllib.request.urlopen('https://www.arabidopsis.org/download_files/Genes/TAIR10_genome_release/TAIR10_chromosome_files/TAIR10_chr_all.fas.gz')
+<<<<<<< HEAD
+	print(datetime.now() + 'Reference file read in.')
+        with gzip.open(ref_url, 'rb') as f_in_ref:
+            with open('TAIR10_chr_all.fas', 'wb') as f_out_ref:
+                shutil.copyfileobj(f_in_ref, f_out_ref)
+	print(datetime.now() + 'Reference file copied')
+=======
         with gzip.open(ref_url, 'rb') as f_in:
             with open('TAIR10_chr_all.fas', 'wb') as f_out:
                 shutil.copyfileobj(f_in, f_out)
+>>>>>>> 591beab4cc858f3213361f14e71615ea781b7540
         expected_return = 'star --runThreadN 6 --runMode genomeGenerate --genomeDir . --genomeFastaFiles SRR18117709_1.fastq SRR18117708_2.fastq --sjdbGTFFile TAIR10_GFF3_genes.gff --sjdbOverhang 100', 'star --runThreadN 4 --runMode alignReads --readFilesIn SRR18117708_1.fastq SRR18117708_2.fastq'
         fastq1 = 'SRR18117708_1.fastq'
         fastq2 = 'SRR18117708_2.fastq'
